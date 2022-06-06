ecf4fa9eaf289dce418d72795e0f31c36c80a407
Timothy Cebula <timothycebula@gmail.com>
2022-06-06 15:02:18 -0400

    Added routes/set up app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Home from './pages/Home';
+import Nav from './pages/Nav';
+import User from './pages/User';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/nav"} element={<Nav />} />
+        <Route path={"/user"} element={<User />} />
+      </Routes> 
+    </BrowserRouter>
   );
 }
 
